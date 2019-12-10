92a8eb828a9e26bd491f47e0c86649e6145359e5
StepanovPlaton <mail2platon@yandex.ru>
2019-12-10 16:25:49 +0400

    Initial commit
    Create repository and project

---
+++ app/src/main/java/com/karandash_and_samodelkin/multimusic/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.karandash_and_samodelkin.multimusic.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
